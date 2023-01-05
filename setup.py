ea454527d9351bbb327a00ba453b0f0b3a56fa26
SchmidtAF <floriaanschmidt@gmail.com>
2023-01-05 23:14:06 +0100

    Bump version: 0.0.1a0 → 0.0.2a0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = ''
 EMAIL = 'amand.schmidt@ucl.ac.uk'
 AUTHOR = 'A Floriaan Schmidt'
 REQUIRES_PYTHON = '>=3.7.0'
-VERSION = '0.0.1a0'
+VERSION = '0.0.2a0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
