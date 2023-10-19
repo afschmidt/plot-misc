bd2c34d90de78114ef1af55c63668a3caa2b2fb0
SchmidtAF <floriaanschmidt@gmail.com>
2023-10-19 17:42:45 +0200

    Added format_roc

--- tests/utils/test_utils.py
+++ tests/utils/test_utils.py
@@ -1,6 +1,7 @@
 """
 testing the `utils` module
 """
+import numpy as np
 from plot_misc.constants import UtilsNames as UNames
 from plot_misc.example_data import examples
 from plot_misc.utils.utils import (
@@ -9,6 +10,7 @@ from plot_misc.utils.utils import (
     calc_matrices,
     _update_kwargs,
     _dict_string_argument,
+    format_roc,
 )
 
 
@@ -165,6 +167,23 @@ class TestCalcMatrices(object):
                              )
         assert res6.curated_matrix_value.iloc[0].to_list() == \
             [0.596, -0.02]
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+class Test_format_roc(object):
+    '''
+    Testing functions for the `format_roc` module
+    '''
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    def test_format_roc(self):
+        observed = np.array([[1, 0, 0, 0, 1, 0, 0, 0]]).T
+        predicted = np.array([[0.3, 0.1, -0.2, 0.11, 0.25,0.001, 0.31, 0.02]]).T
+        sample_weights = np.array([[10,1, 2, 20, 1,  2, 2, 2]]).T
+        # results
+        res1 = format_roc(observed, predicted)
+        res2 = format_roc(observed, predicted, sample_weight=sample_weights)
+        # assert
+        assert res1.mean().round(2).tolist() == [0.33, 0.5, 0.42]
+        assert res2.mean().round(2).tolist() == [0.39, 0.7, 0.31]
 
 
 
