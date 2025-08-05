46fe084905a97fe8dcb94de06c902cd7dbf6e811
SchmidtAF <floriaanschmidt@gmail.com>
2025-08-05 22:02:58 +0200

    Updated documentation and examples

--- plot_misc/volcano.py
+++ plot_misc/volcano.py
@@ -113,6 +113,15 @@ def plot_volcano(data:DataFrame, y_column:str, x_column:str,
         The created figure (if `ax` was not provided).
     ax : `matplotlib.axes.Axes`
         The matplotlib axis containing the plot.
+    
+    Notes
+    -----
+    When `adjust=True`, text labels are repositioned using the `adjustText`
+    package to reduce overlaps. This is particularly helpful when multiple
+    points are labelled in a crowded region. Additional options can be passed
+    to `adjustText` via `label_kwargs_dict`.
+    
+    Please see the module documentation `here <https://pypi.org/project/adjustText/>`_.
     """
     FT_FAM = 'font.family'
     # ###### Check input
