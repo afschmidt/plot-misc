1e00c0951f56b05ce68e91b5417afe21af268eac
SchmidtAF <floriaanschmidt@gmail.com>
2026-04-07 20:37:54 +0200

    refactor(ci): unified dependencies and removed stale code/comments
        - Add sphinx, themes, nbsphinx, numpydoc etc. to requirements-dev.txt
        - Delete resources/ci_cd/sphinx.txt
        - Remove corresponding pip install line from before_script.sh
        - Strip defunct image processing and data-dict blocks from pages.sh
        - Remove SSH key setup boilerplate from run_docker.sh
        - Update project name references (merit/gwas_norm -> plot-misc)

--- resources/ci_cd/pages.sh
+++ resources/ci_cd/pages.sh
@@ -5,31 +5,11 @@ echo "[pages] running in: $PWD"
 # Ensure the documentation build directory is present
 mkdir -p ./docs/build
 
-# Copy the current README over and change any image locations for it
-# NOTE currently no images are used.
-# cp README.md docs/source/getting_started.md
-# sed -i 's/\.\/docs\/source\///' docs/source/getting_started.md
-# sed -i 's|src="\./resources/images|src="./_static/images|'  docs/source/getting_started.md
-
-# Process any data dictionary files
-# NOTE USED TO PARSE TABLES 
-# mkdir -p docs/source/data_dict
-# for i in ./resources/data_dict/*.txt; do
-#     outfile="$(basename "$i" | sed 's/\.txt/.rst/')"
-#     doc-column-list "$i" ./docs/source/data_dict/"$outfile"
-# done
-
-# Copy the images over to the source static directory
-# mkdir -p docs/source/_static
-# cp ./resources/images/*.png ./docs/source/_static/
-
 cd docs
 make html
 cd ..
 echo "[pages] removing public"
-
-echo "[pages] removing public"
-if [[ -e public ]]; then
+if [ -e public ]; then
     rm -r public
 fi
 echo "[pages] creating public"
